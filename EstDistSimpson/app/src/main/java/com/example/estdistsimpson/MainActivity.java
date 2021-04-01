46927d4ed86fedb28285c3cf08f63b5d8136541b
ohway4death <bigplant9411@chiba-u.jp>
2021-04-01 22:18:45 +0900

    20210401の進捗

--- EstDistSimpson/app/src/main/java/com/example/estdistsimpson/MainActivity.java
+++ EstDistSimpson/app/src/main/java/com/example/estdistsimpson/MainActivity.java
@@ -1,26 +1,23 @@
 package com.example.estdistsimpson;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.pm.ActivityInfo;
-import android.os.Bundle;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.os.SystemClock;
-import android.widget.Button;
+import android.os.Bundle;
 import android.widget.TextView;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener{
 
     private SensorManager sensorManager;
     private Sensor accel;
     private TextView textViewTitle, textViewAx, textViewAy, textViewAz;
-    private TextView textViewPeriod, textViewSpeed, textViewDistance;
+    private TextView textViewCount, textViewQueue, textViewDistance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,8 +34,8 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         textViewAz = findViewById(R.id.text_view_Az);
 
         textViewDistance = findViewById(R.id.distance);
-        textViewSpeed = findViewById(R.id.speed);
-        textViewPeriod = findViewById(R.id.period);
+        textViewQueue = findViewById(R.id.queue);
+        textViewCount = findViewById(R.id.count);
 
         //SystemClock.elapsedRealtimeNanos()はスマホが起動してからの時間を表示する，単位はナノ秒
         //period.setText((SystemClock.elapsedRealtimeNanos()/1e9)+"");
@@ -70,6 +67,15 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     float sensorAx_bef = 0;
     float sensorAy_bef = 0;
     float sensorAz_bef = 0;
+    Queue<Float> queueAx = new ArrayBlockingQueue<>(3);
+    Queue<Float> queueAy = new ArrayBlockingQueue<>(3);
+    Queue<Float> queueAz = new ArrayBlockingQueue<>(3);
+    float time_bef = 0;
+    float period = 0;
+
+
+    int count_acc = 0;
+    int count_spd = 0;
 
 
     @Override
@@ -78,7 +84,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         //計算はここの外側で行う
         //センサーのイベントを処理する部分
 
-
         switch (event.sensor.getType()){
             case Sensor.TYPE_ACCELEROMETER:
 
@@ -86,7 +91,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                 sensorAy = event.values[1];
                 sensorAz = event.values[2];
 /*
-
                 float values_filter[] = calculateDistance(sensorAx, sensorAy, sensorAz, event.timestamp);
                 float values_origin[] = calculateDistance(event.values[0], event.values[1], event.values[2], event.timestamp);
 */
@@ -94,20 +98,62 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                 textViewAy.setText(String.format("%.3f",sensorAy));
                 textViewAz.setText(String.format("%.3f",sensorAz));
 
-                sensorAx_bef = sensorAx;
-                sensorAy_bef = sensorAy;
-                sensorAz_bef = sensorAz;
+                //キューがいっぱいなら先頭のやつ1つ消す
+                if (queueAx.size() == 3 && queueAy.size() == 3 && queueAz.size() == 3 ){
+                    queueAx.poll();
+                    queueAy.poll();
+                    queueAz.poll();
+                }
+                //キューに代入
+                queueAx.offer(sensorAx);
+                queueAy.offer(sensorAy);
+                queueAz.offer(sensorAz);
+                //最初に計測した奴を代入しておく
+                if (count_acc== 1){
+                    time_bef = event.timestamp;
+                }
+                //キューに3つの要素があり，カウントが奇数の時，シンプソンをつかう
+                if (queueAx.size() == 3 && count_acc%2 == 1){
+                    Float[] array = new Float[3];
+                    queueAx.toArray(array);
+                    period = event.timestamp - time_bef;
+                    estDist(array, period);
+                }
 
                 break;
         }
 
-
-
+        textViewQueue.setText(String.valueOf(queueAx));
+        textViewCount.setText(String.valueOf(count_acc));
+        count_acc++;
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
         //センサー精度の変更を行うときに利用するメソッド
+    }
+
+    Queue<Float> queueVx = new ArrayBlockingQueue<>(3);
+
+    public float simpson(Float[] str, float diff){
+        return (str[0] + str[2] + 4 * str[1])*diff/6;
+    }
+
+    public void estDist(Float[] accel, float diff){
+        if (queueVx.size() == 3 ){
+            queueAx.poll();
+        }
+        queueVx.offer(simpson(accel, diff));
+
+        //キューに3つの要素があり，カウントが奇数の時，シンプソンをつかう
+        if (queueVx.size() == 3 && count_spd%2 == 1){
+            Float[] array_vx = new Float[3];
+            queueVx.toArray(array_vx);
+            period = event.timestamp - time_bef;
+            estDistSimpson(array, period);
+        }
+
+
     }
 
 
